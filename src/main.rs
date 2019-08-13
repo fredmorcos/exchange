153d40731271d62a13da791de4b8b1a28569ccc7
Fred Morcos <fred.morcos@gmail.com>
2019-08-13 11:44:43 +0200

    Initial main and statusln! implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::all)]
+
+use std::io::{self, BufRead};
+macro_rules! statusln {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)] {
+            eprint!("[STATUS] ");
+            eprintln!($($arg)*);
+        }
+    };
+}
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdin_handle = stdin.lock();
+
+    let mut buffer = String::new();
+
+    loop {
+        match stdin_handle.read_line(&mut buffer) {
+            Ok(0) => {
+                statusln!("Closing...");
+                break;
+            }
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error: {}, closing...", e);
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
